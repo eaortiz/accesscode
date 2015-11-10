85f5babb100c31014298a597b975b14a18263d3b
Estefania Ortiz <eaortiz@stanford.edu>
2015-11-10 12:08:14 -0500

    Update Recursion.java

--- AlgorithmHW/src/nyc/c4q/sufeiiz/Recursion.java
+++ AlgorithmHW/src/nyc/c4q/sufeiiz/Recursion.java
@@ -7,12 +7,13 @@ public class Recursion {
     public static boolean isEvenWord(String word) {
         if (word.length() == 0)
             return true;
+        // I like this other base case, it's not necessary but with very long words it will save time.
         else if (word.length() % 2 != 0)
             return false;
         else {
             char letter = word.charAt(0);
             word = word.substring(1);
-
+            // add a variable for Character.toString(letter)
             if (word.contains(Character.toString(letter))) {
                 word = word.replaceFirst(Character.toString(letter), "");
                 return isEvenWord(word);
@@ -26,4 +27,6 @@ public class Recursion {
         System.out.println(isEvenWord("appeases"));
         System.out.println(isEvenWord("banana"));
     }
+    
+    //good and clean code, but think about more test cases
 }
